20f9071f291b27dfc663af23531869fe7350a8a7
Caleb Barger <bargercj20@hotmail.com>
2020-05-26 10:30:42 -0700

    redux setup

---
+++ rent-my-tech/src/store/reducers/index.js
@@ -0,0 +1,10 @@
+import {combineReducers} from 'redux'
+import {exampleReducer} from './exampleReducer'
+// import reducer2 from './reducer2'
+
+export default combineReducers({
+    exampleReducer,
+    // reducer2,
+    // reducer3,
+    // etc...
+})
