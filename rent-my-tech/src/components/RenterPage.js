96db418a16cc58ced12a8f43e891e605787cd314
Caleb Barger <bargercj20@hotmail.com>
2020-05-26 12:51:34 -0700

    added RenterPage and RenterPageInfo

---
+++ rent-my-tech/src/components/RenterPage.js
@@ -0,0 +1,2 @@
+// this is where renter info will be displayed and a list of items to rent is also shown
+// 
---
+++ rent-my-tech/src/components/RenterPageInfo.js
@@ -0,0 +1,3 @@
+// here the renter will be able to see what item they clicked on in greater detail
+// and will be able to make a request on that item
+// after the request is updated by the put request the user will be taken back to the RenterPage component
