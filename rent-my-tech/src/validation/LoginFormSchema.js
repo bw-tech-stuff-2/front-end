52c033e8a02f49ed1e70de7be6cd38e8781e69e3
Simon <simonh72498@gmail.com>
2020-05-27 01:05:36 -0400

    fixed login schema

--- rent-my-tech/src/validation/LoginFormSchema.js
+++ rent-my-tech/src/validation/LoginFormSchema.js
@@ -3,9 +3,11 @@ import * as yup from "yup";
 const LoginFormSchema = yup.object().shape({
     username: yup.string()
         .trim()
+        .min(4, "The username must be at least four characters long.")
         .required("The username is a required field."),
     password: yup.string()
         .trim()
+        .min(6, "Your password must be at least 6 characers long.")
         .required("The password is a required field."),
     fullName: yup.string()
         .trim()
