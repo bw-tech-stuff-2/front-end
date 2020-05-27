e225686807545efabeb6b6444e227784d7ebe0dc
Simon <simonh72498@gmail.com>
2020-05-27 16:22:31 -0400

    added styling to forms

--- rent-my-tech/src/App.js
+++ rent-my-tech/src/App.js
@@ -3,33 +3,47 @@ import './App.css';
 import SignupForm from "./components/SignupForm";
 import LoginForm from "./components/LoginForm";
 import {Route, NavLink} from "react-router-dom";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import styled from "styled-components";
+
+const StyledFormDiv = styled.div`
+  margin: 100px 0 0 0;
+  display: flex;
+  flex-flow: column;
+  text-align: center;
+`;
 
 function App() {
   const [token, setToken] = useState("");
   return (
     <div>
       <Route exact path = "/">
-        <nav>
-          <NavLink className="homeLink" to="/">Home</NavLink>
-          <NavLink className="signupLink" to="/register">Register</NavLink>
-          <NavLink className="loginLink" to="/login">Login</NavLink>
-        </nav>
+        <AppBar>
+          <Button component={NavLink} color="inherit" className="homeLink" to="/">Home</Button>
+          <Button component={NavLink} color="inherit" className="signupLink" to="/register">Register</Button>
+          <Button component={NavLink} color="inherit" className="loginLink" to="/login">Login</Button>
+        </AppBar>
       </Route>
       <Route path="/register">
-        <nav>
-          <NavLink className="homeLink" to="/">Home</NavLink>
-          <NavLink className="signupLink" to="/register">Register</NavLink>
-          <NavLink className="loginLink" to="/login">Login</NavLink>
-        </nav>
+        <AppBar>
+          <Button component={NavLink} color="inherit" className="homeLink" to="/">Home</Button>
+          <Button component={NavLink} color="inherit" className="signupLink" to="/register">Register</Button>
+          <Button component={NavLink} color="inherit" className="loginLink" to="/login">Login</Button>
+        </AppBar>
+        <StyledFormDiv>
         <SignupForm />
+        </StyledFormDiv>
       </Route>
       <Route path="/login">
-        <nav>
-          <NavLink className="homeLink" to="/">Home</NavLink>
-          <NavLink className="signupLink" to="/register">Register</NavLink>
-          <NavLink className="loginLink" to="/login">Login</NavLink>
-        </nav>
+        <AppBar>
+          <Button component={NavLink} color="inherit" className="homeLink" to="/">Home</Button>
+          <Button component={NavLink} color="inherit" className="signupLink" to="/register">Register</Button>
+          <Button component={NavLink} color="inherit" className="loginLink" to="/login">Login</Button>
+        </AppBar>
+        <StyledFormDiv>
         <LoginForm token={token} setToken={setToken}/>
+        </StyledFormDiv>
       </Route>
     </div>
   );
